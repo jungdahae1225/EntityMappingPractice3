0f0da8e3eaffb0baa7f1ba36ce50a371ac3d0e01
jungdahae1225 <jungdahae1225@gmail.com>
2021-09-04 00:05:12 +0900

    valueType Mapping

---
+++ src/main/java/jpashop/domain/Address.java
@@ -0,0 +1,50 @@
+package jpashop.domain;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public String getCity() {
+        return city;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    private void setStreet(String street) { //privated으로 setter로 값 접근 하는 걸 최대한 막기
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    //==참고 jpa에서는 항상 프록시의 사용을 감안하여 getter로 접근하는 메서드로 코딩하자.==//
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
